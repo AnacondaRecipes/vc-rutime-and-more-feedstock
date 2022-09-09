b4ed8476095387cec79591ef0c6cb653f657f795
Kai Tietz <ktietz@anaconda.com>
2022-09-09 18:21:10 +0200

    adjust batches ...

--- recipe/bld_vc_runtime.bat
+++ recipe/bld_vc_runtime.bat
@@ -1,4 +1,5 @@
 echo "Vars are: %runtime_version%, %target_platform%"
 
-python -m vc_repack --extract --version %runtime_version% -a %target_platform% %RECIPE_DIR%\vc_repack.py
+copy /b %RECIPE_DIR%\vc_repack.py .
+python -m vc_repack --extract --version %runtime_version% -a %target_platform% vc_repack.py
 
